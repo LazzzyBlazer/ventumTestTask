43fd5e862abba2e7a9e518943f67f6c14c0d8c2a
LazzzyBlazer <lazzzyblazer@gmail.com>
2021-07-09 12:58:38 +0200

    v: 0.0.3
    -Clicker func

---
+++ Coin Test/Assets/Scripts/Clicker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Clicker : MonoBehaviour
+{
+    [SerializeField] int counter = 30;
+    public TMP_Text counterText;
+
+    public void ButtonClick(){
+        if(counter == 0){
+            counter = 30;
+        }
+        else{
+            counter--;
+        }
+    }
+
+    void Update()
+    {
+        counterText.text = "Монеток: " + counter.ToString();
+    }
+}
